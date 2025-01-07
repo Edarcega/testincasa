e781fc9e474b70932953dd89bac13226eb2c6e8a
edimar <edimar.eds@gmail.com>
2025-01-07 14:25:57 -0300

    Projeto criado

---
+++ src/main/java/com/edstecno/testincasa/testincasa/TestincasaApplication.java
@@ -0,0 +1,13 @@
+package com.edstecno.testincasa.testincasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestincasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestincasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edstecno/testincasa/testincasa/TestincasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edstecno.testincasa.testincasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestincasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
